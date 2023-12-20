e304710d87a71ce80a4d1806fd6cea0318262d48
JoeHadf <151024605+JoeHadf@users.noreply.github.com>
2023-12-20 17:22:31 +0000

    Add files via upload

---
+++ AOCDay19.cs
@@ -0,0 +1,219 @@
+﻿string input = File.ReadAllText(@"C:\Users\Josep\Documents\AOC2023\Day19Input.txt");
+
+string[] inputParts = input.Split("\r\n\r\n");
+
+string[] stringWorkflows = inputParts[0].Split("\r\n");
+string[] stringParts = inputParts[1].Split("\r\n");
+
+Dictionary<string, Workflow> workflows = new Dictionary<string, Workflow>(stringWorkflows.Length);
+
+char[] deliminators = new char[] { '{', '}', ',' };
+
+for (int i = 0; i < stringWorkflows.Length; i++)
+{
+    string currentStringWorkflow = stringWorkflows[i];
+    string[] rules = currentStringWorkflow.Split(deliminators, StringSplitOptions.RemoveEmptyEntries);
+
+    List<Rule> workflowRules = new List<Rule>();
+
+    for (int j = 1; j < rules.Length - 1; j++)
+    {
+        string currentRule = rules[j];
+
+        string[] splitRule = currentRule.Split(":");
+        
+        workflowRules.Add(new Rule(splitRule[0][0], splitRule[0][1], long.Parse(splitRule[0][2..]), splitRule[1]));
+    }
+
+    Workflow currentWorkflow = new Workflow(workflowRules, rules[^1]);
+    
+    workflows.Add(rules[0], currentWorkflow);
+}
+
+List<BoundGroup> acceptedBounds = new List<BoundGroup>();
+
+List<Workflow> foundWorkflows = new List<Workflow>(workflows.Count);
+foundWorkflows.Add(workflows["in"]);
+
+while (foundWorkflows.Count > 0)
+{
+    Workflow currentWorkflow = foundWorkflows[0];
+    foundWorkflows.RemoveAt(0);
+
+    BoundGroup defaultBoundGroup = new BoundGroup();
+    defaultBoundGroup.Encapsulate(currentWorkflow.boundGroup);
+
+    for (int i = 0; i < currentWorkflow.rules.Count; i++)
+    {
+        Rule currentRule = currentWorkflow.rules[i];
+        if (workflows.TryGetValue(currentRule.nextWorkflow, out Workflow? nextWorkflow))
+        {
+            nextWorkflow.boundGroup.Encapsulate(currentWorkflow, defaultBoundGroup, currentRule);
+            foundWorkflows.Add(nextWorkflow);
+        }
+
+        if (currentRule.nextWorkflow == "A")
+        {
+            BoundGroup currentBoundGroup = new BoundGroup();
+            currentBoundGroup.Encapsulate(currentWorkflow, defaultBoundGroup, currentRule);
+            
+            acceptedBounds.Add(currentBoundGroup);
+        }
+
+        defaultBoundGroup.Encapsulate(currentRule.GetInvertedRule());
+    }
+    
+    if (workflows.TryGetValue(currentWorkflow.defaultWorkflow, out Workflow? defaultWorkflow))
+    {
+        defaultWorkflow.boundGroup.Encapsulate(currentWorkflow, defaultBoundGroup, new Rule());
+        foundWorkflows.Add(defaultWorkflow);
+    }
+    
+    if (currentWorkflow.defaultWorkflow == "A")
+    {
+        BoundGroup currentBoundGroup = new BoundGroup();
+        currentBoundGroup.Encapsulate(currentWorkflow, defaultBoundGroup, new Rule());
+        
+        acceptedBounds.Add(currentBoundGroup);
+    }
+}
+
+long ratingCombinations = 0;
+
+for (int i = 0; i < acceptedBounds.Count; i++)
+{
+    ratingCombinations += acceptedBounds[i].GetSize();
+}
+
+Console.WriteLine($"Rating Combinations : {ratingCombinations}");
+
+class Workflow
+{
+    public List<Rule> rules;
+    public string defaultWorkflow;
+
+    public BoundGroup boundGroup;
+    public Workflow(List<Rule> rules, string defaultWorkflow)
+    {
+        this.rules = rules;
+        this.defaultWorkflow = defaultWorkflow;
+
+        this.boundGroup = new BoundGroup();
+    }
+}
+
+struct Rule
+{
+    public char category;
+    public char inequality;
+    public long bound;
+    public string nextWorkflow;
+
+    public Rule(char category, char inequality, long bound, string nextWorkflow)
+    {
+        this.category = category;
+        this.inequality = inequality;
+        this.bound = bound;
+        this.nextWorkflow = nextWorkflow;
+    }
+
+    public Rule()
+    {
+        this.category = ' ';
+        this.inequality = ' ';
+        this.bound = -1;
+        this.nextWorkflow = "";
+    }
+
+    public Rule GetInvertedRule()
+    {
+        if (inequality == '<')
+        {
+            return new Rule(category, '>', bound - 1, "");
+        }
+
+        if (inequality == '>')
+        {
+            return new Rule(category, '<', bound + 1, "");
+        }
+
+        return new Rule();
+    }
+}
+
+class Bound
+{
+    public long lower = 0;
+    public long upper = 4001;
+
+    public void Encapsulate(Bound otherBound)
+    {
+        this.lower = Math.Max(lower, otherBound.lower);
+        this.upper = Math.Min(upper, otherBound.upper);
+    }
+
+    public void Encapsulate(char inequality, long bound)
+    {
+        if (inequality == '<')
+        {
+            this.upper = Math.Min(upper, bound);
+        }
+
+        if (inequality == '>')
+        {
+            this.lower = Math.Max(lower, bound);
+        }
+    }
+    
+    public long GetSize()
+    {
+        return upper - lower - 1;
+    }
+}
+
+class BoundGroup
+{
+    public Bound xBound = new Bound();
+    public Bound mBound = new Bound();
+    public Bound aBound = new Bound();
+    public Bound sBound = new Bound();
+
+    public void Encapsulate(BoundGroup otherWorkflow)
+    {
+        xBound.Encapsulate(otherWorkflow.xBound);
+        mBound.Encapsulate(otherWorkflow.mBound);
+        aBound.Encapsulate(otherWorkflow.aBound);
+        sBound.Encapsulate(otherWorkflow.sBound);
+    }
+
+    public void Encapsulate(Rule rule)
+    {
+        switch (rule.category)
+        {
+            case 'x':
+                xBound.Encapsulate(rule.inequality, rule.bound);
+                break;
+            case 'm':
+                mBound.Encapsulate(rule.inequality, rule.bound);
+                break;
+            case 'a':
+                aBound.Encapsulate(rule.inequality, rule.bound);
+                break;
+            case 's':
+                sBound.Encapsulate(rule.inequality, rule.bound);
+                break;
+        }
+    }
+    
+    public void Encapsulate(Workflow prevWorkflow, BoundGroup defaultBounds, Rule rule)
+    {
+        Encapsulate(prevWorkflow.boundGroup);
+        Encapsulate(defaultBounds);
+        Encapsulate(rule);
+    }
+
+    public long GetSize()
+    {
+        return xBound.GetSize() * mBound.GetSize() * aBound.GetSize() * sBound.GetSize();
+    }
+}
---
+++ AOCDay20.cs
@@ -0,0 +1,215 @@
+﻿string[] input = File.ReadAllLines(@"C:\Users\Josep\Documents\AOC2023\Day20Input.txt");
+char[] deliminators = new char[] { ' ', '-', '>', ',' };
+
+Dictionary<string, Module> modules = new Dictionary<string, Module>(input.Length);
+
+for (int i = 0; i < input.Length; i++)
+{
+    string[] inputData = input[i].Split(deliminators, StringSplitOptions.RemoveEmptyEntries);
+    
+    Module currentModule;
+    string name;
+    
+    if (inputData[0][0] == '%')
+    {
+        currentModule = new FlipFlop();
+        name = inputData[0][1..];
+
+    }
+    else if (input[i][0] == '&')
+    {
+        currentModule = new Conjunction();
+        name = inputData[0][1..];
+    }
+    else
+    {
+        currentModule = new Broadcaster();
+        name = inputData[0];
+    }
+
+    List<string> moduleDestinations = new List<string>(inputData.Length - 1);
+
+    for (int j = 1; j < inputData.Length; j++)
+    {
+        moduleDestinations.Add(inputData[j]);
+    }
+
+    currentModule.destinations = moduleDestinations;
+    currentModule.name = name;
+    
+    modules.Add(name, currentModule);
+}
+
+foreach(KeyValuePair<string, Module> module in modules)
+{
+    if (module.Value is Conjunction conjunction)
+    {
+        foreach (KeyValuePair<string, Module> inputModule in modules)
+        {
+            if (inputModule.Value.destinations.Contains(module.Key))
+            {
+                conjunction.rememberedPulses.Add(inputModule.Key, PulseType.Low);
+            }
+        }
+    }
+}
+
+List<Pulse> pulsesToDo = new List<Pulse>();
+
+int lowCount = 0;
+int highCount = 0;
+
+bool hasFoundButtonPressCount = false;
+
+string[] parentModules = new string[] {"lh", "fk", "ff", "mm"};
+
+Dictionary<string, int> pressesToReceiveLow = new Dictionary<string, int>();
+
+int pressCount = 1;
+
+while(!hasFoundButtonPressCount)
+{
+    pulsesToDo.Add(new Pulse(PulseType.Low,"button" ,"broadcaster"));
+
+    while (pulsesToDo.Count > 0)
+    {
+        Pulse currentPulse = pulsesToDo[0];
+        pulsesToDo.RemoveAt(0);
+
+        if (currentPulse.type == PulseType.Low)
+        {
+            lowCount++;
+        }
+        else
+        {
+            highCount++;
+        }
+
+        string pulseDestination = currentPulse.destination;
+
+        if (modules.TryGetValue(pulseDestination, out Module? currentModule))
+        {
+            pulsesToDo.AddRange(currentModule.ReceivePulse(currentPulse.inputModule, currentPulse.type));
+        }
+
+        if (currentPulse.type == PulseType.Low && parentModules.Contains(currentPulse.destination))
+        {
+            Console.WriteLine($"Low pulse received at {currentPulse.destination} after {pressCount} presses");
+            pressesToReceiveLow.TryAdd(currentPulse.destination, pressCount);
+        }
+    }
+
+    bool allParentsPressesFound = true;
+    for (int i = 0; i < parentModules.Length; i++)
+    {
+        if (!pressesToReceiveLow.ContainsKey(parentModules[i]))
+        {
+            allParentsPressesFound = false;
+        }
+    }
+
+    hasFoundButtonPressCount = allParentsPressesFound;
+
+    pressCount++;
+}
+
+long pressMultiplied = 1;
+
+for (int i = 0; i < parentModules.Length; i++)
+{
+    pressMultiplied *= pressesToReceiveLow[parentModules[i]];
+}
+
+Console.WriteLine($"Single Low Pulse To rx : {pressMultiplied}");
+
+class Module
+{
+    public string name;
+    
+    public List<string> destinations = new List<string>();
+
+    private protected List<Pulse> GetPulseList(PulseType type)
+    {
+        List<Pulse> pulses = new List<Pulse>(destinations.Count);
+        
+        for (int i = 0; i < destinations.Count; i++)
+        {
+            pulses.Add(new Pulse(type, name,destinations[i]));
+        }
+
+        return pulses;
+    }
+
+    public virtual List<Pulse> ReceivePulse(string inputModule, PulseType type)
+    {
+        return new List<Pulse>();
+    }
+}
+
+class FlipFlop : Module
+{
+    private bool isOn = false;
+    
+    public override List<Pulse> ReceivePulse(string inputModule, PulseType type)
+    {
+        if (type == PulseType.Low)
+        {
+            isOn = !isOn;
+            PulseType typeToSend = (isOn) ? PulseType.High: PulseType.Low;
+            return GetPulseList(typeToSend);
+        }
+
+        return new List<Pulse>();
+    }
+}
+
+class Conjunction : Module
+{
+    public Dictionary<string, PulseType> rememberedPulses = new Dictionary<string, PulseType>();
+    public override List<Pulse> ReceivePulse(string inputModule, PulseType type)
+    {
+        rememberedPulses[inputModule] = type;
+
+        bool allHigh = true;
+
+        foreach (PulseType rememberedPulse in rememberedPulses.Values)
+        {
+            if (rememberedPulse == PulseType.Low)
+            {
+                allHigh = false;
+            }
+        }
+
+        PulseType typeToSend = (allHigh) ? PulseType.Low : PulseType.High;
+
+        return GetPulseList(typeToSend);
+    }
+}
+
+class Broadcaster : Module
+{
+    public override List<Pulse> ReceivePulse(string inputModule, PulseType type)
+    {
+        return GetPulseList(type);
+    }
+}
+
+struct Pulse
+{
+    public PulseType type;
+    public string inputModule;
+    public string destination;
+
+    public Pulse(PulseType type, string inputModule, string destination)
+    {
+        this.type = type;
+        this.inputModule = inputModule;
+        this.destination = destination;
+    }
+}
+
+enum PulseType
+{
+    Low,
+    High
+}
